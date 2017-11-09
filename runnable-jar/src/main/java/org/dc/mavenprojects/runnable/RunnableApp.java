ca1ff6a00e2034fb973e646b2c73da12b6b42b1d
Digital Alchemist <developer.anwar@gmail.com>
2017-11-09 09:31:55 -0500

    Maven Plugins
    1- Dependency Plugin
    2- Maven Jar Plugin

---
+++ runnable-jar/src/main/java/org/dc/mavenprojects/runnable/RunnableApp.java
@@ -0,0 +1,11 @@
+package org.dc.mavenprojects.runnable;
+
+/**
+ * Hello world!
+ *
+ */
+public class RunnableApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World! : Its a runnable jar");
+	}
+}
