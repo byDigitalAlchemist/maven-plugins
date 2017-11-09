57fe22958dc2d5077a6582ef98297e270b91cd8a
Digital Alchemist <developer.anwar@gmail.com>
2017-11-09 09:56:01 -0500

    Joda Time Library
    1- Added dependency Joda Time

--- runnable-assembly-plugin/src/main/java/org/dc/mavenprojects/plugin/assembly/RunnableAssemblyPlugin.java
+++ runnable-assembly-plugin/src/main/java/org/dc/mavenprojects/plugin/assembly/RunnableAssemblyPlugin.java
@@ -1,10 +1,9 @@
 package org.dc.mavenprojects.plugin.assembly;
 
+import org.joda.time.DateTimeZone;
 
-public class RunnableAssemblyPlugin 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! :: Runnable Jar Via Maven Assembly Plugin " );
-    }
+public class RunnableAssemblyPlugin {
+	public static void main(String[] args) {
+		System.out.println("Hello World! :: Runnable Jar Via Maven Assembly Plugin :: Using Joda :: " + DateTimeZone.getDefault());
+	}
 }
